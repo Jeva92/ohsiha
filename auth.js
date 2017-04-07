acff08ce66ce52367b69eb3fe4bd1b4fdd994edd
Jeva <jeva92@gmail.com>
2017-04-07 12:01:30 +0300

    auth.js

---
+++ auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'googleAuth' : {
+        'clientID' : 'AppID', // your AppID
+        'clientSecret'  : 'AppSecret', // your App Secret
+        'callbackURL' : 'http://localhost:8000/auth/google/callback'
+    }
+};
