3f0cfaed71dbcc2fdb56ebef2e7c3acbaaa771cf
Jeva92 <Jeva92@users.noreply.github.com>
2017-04-07 11:57:42 +0300

    fix

--- auth.js
+++ auth.js
@@ -1,8 +1,8 @@
 module.exports = {
 
     'googleAuth' : {
-        'clientID' : '1094454047804-jsnv2m131jhes2lbttblgl0ashf3u73q.apps.googleusercontent.com', // your AppID
-        'clientSecret'  : '2to_GWuSjuClCiAunYUVGzlA', // your App Secret
+        'clientID' : 'AppID', // your AppID
+        'clientSecret'  : 'AppSecret', // your App Secret
         'callbackURL' : 'http://localhost:8000/auth/google/callback'
     }
 };
